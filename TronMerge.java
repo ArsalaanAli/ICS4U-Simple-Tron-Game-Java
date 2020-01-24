f86a46cc0a23795f9a0b9a7e089461504a37f0da
arsalaanAli <38818326+ArsalaanAli@users.noreply.github.com>
2020-01-23 21:06:12 -0500

    EVERYTHING
    ADDED EVERYTHING THATS BEEN DONE FOR PAST WEEK

--- TronMerge.java
+++ TronMerge.java
@@ -80,7 +80,7 @@ class Tron extends JFrame{
 			if(game!= null && game.ready){
                 game.timer();
                 if(running){
-                    game.move();//GAME FUNCTIONS ARE CALLED
+                    // game.move();//GAME FUNCTIONS ARE CALLED
                     game.enemyAI();
                     game.repaint();
                     if(game.checkFinished() != 0){
@@ -106,15 +106,17 @@ class GamePanel1 extends JPanel{
     public static final int LEFT = 1, RIGHT = 2, UP = 3, DOWN =4;
     public static final int WIN = 1, LOSE = 2, TIE = 3;
     public static final int playerTrail = 5, enemyTrail = 6;
+    private int backDelay = 0;
     public final int width = 800, height = 600;
     private int[][] board = new int[170][150];
     private boolean []keys;
     private boolean playerDead = false, enemyDead = false, tie = false;
     private boolean drawBackground = true;
     private bike player, enemy;
-    private int time = 1, ms = 0, randTimeLow = 4, randTimeHigh = 15;
-    private int timeChangeDir = randint(randTimeLow, randTimeHigh);
+    private int time = 1, ms = 0, randTimeLow = 15, randTimeHigh = 17;
+    private int timeChangeDir = randint(15, 20);
     public boolean ready = false;
+    private int[] otherTurns = {0, RIGHT, LEFT, DOWN, UP};
     public GamePanel1(){
         player = new bike(50, 320, RIGHT);
         enemy = new bike(600, 320, LEFT);
@@ -128,8 +130,6 @@ class GamePanel1 extends JPanel{
         ready = true;
     }
     public void move(){
-       player.move();
-       player.setDir();
        if(keys[KeyEvent.VK_S]){
            player.addDir(DOWN);
        }
@@ -141,6 +141,11 @@ class GamePanel1 extends JPanel{
         }
         if(keys[KeyEvent.VK_D]){
             player.addDir(RIGHT);
+       }
+       player.setDir();
+       player.move();
+       if (player.hitEdgde()){
+           playerDead = true;
        }
        if(collide(player, enemyTrail)){
            if(tie()){
@@ -156,22 +161,26 @@ class GamePanel1 extends JPanel{
        fillBoard(player, playerTrail);
     }
     public void enemyAI(){
-        enemy.move();
-        enemy.setDir();
-        
+        enemyDetection();
         enemy.closeToBorder();
         if(time >= timeChangeDir){
             if(enemy.onBorder()){
-                enemy.addDir(enemy.borderTurn());
+                int dc = enemy.borderTurn();
+                enemy.addDir(dc);
                 time = 0;
                 timeChangeDir = randint(randTimeLow, randTimeHigh);
             }
             else{
-                enemy.changeDir();
+                int dc = enemy.changeDir();
+                if (safeToTurn(dc)){
+                    enemy.addDir(dc);
+                }
                 time = 0;
                 timeChangeDir = randint(randTimeLow, randTimeHigh);
             }
         }
+        enemy.setDir();
+        enemy.move();
         if(collide(enemy, playerTrail)){
             if(tie()){
                 tie = true;
@@ -183,20 +192,30 @@ class GamePanel1 extends JPanel{
         if(collide(enemy, enemyTrail)){
              enemyDead = true;
         }
+        if (enemy.hitEdgde()){
+            enemyDead = true;
+        } 
         fillBoard(enemy, enemyTrail);
     }
+
+
     public void paint(Graphics g){
         if(drawBackground){
             g.setColor(new Color(0, 0, 45));
             g.fillRect(0, 0, width + 30, height + 30);
-            if(time>1){
+            if(backDelay>2){
                 drawBackground = false;
             }
+            backDelay++;
         }
         g.setColor(new Color(187,155,252));
         player.draw(g);
-        g.setColor(new Color(253,255,161));
+        g.setColor(new Color(253, 255,161));
         enemy.draw(g);
+    }
+    public void draw(Graphics g){
+        g.setColor(new Color(0, 0, 45));
+        g.fillRect(0, 0, width+30, height+30);
     }
     public void timer(){
         ms++;
@@ -231,8 +250,60 @@ class GamePanel1 extends JPanel{
         }
         return 0;
     }
+    public void enemyDetection(){
+        if(board[(enemy.getX()+enemy.getxDir()*4)/5][(enemy.getY()+enemy.getyDir()*4)/5] != 0){
+            int dc = enemy.changeDir();
+                if (safeToTurn(dc)){
+                    enemy.addDir(dc);
+                }
+                else{
+                    enemy.addDir(otherTurns[dc]);
+                }
+        }
+        else if( (enemy.getX()+enemy.getxDir()*5)/5 < 5 || (enemy.getX()+enemy.getxDir()*5)/5 > 165){
+            int dc = enemy.changeDir();
+                if (safeToTurn(dc)){
+                    enemy.addDir(dc);
+                }
+                else{
+                    enemy.addDir(otherTurns[dc]);
+                }
+        }
+    }
+    public boolean safeToTurn(int dc){
+        boolean stt = true;
+        // System.out.println(dc +" TURNING");
+        //FINISH: SEE IF SAFE TO RANDOMLY TURN
+        int potentialX = xSpeed(dc);
+        int potentialY = ySpeed(dc);
+        for(int i = 1; i<=4; i++){
+            // System.out.println((enemy.getX()+potentialX*i)/5 + " " + (enemy.getY()+potentialY*i)/5);
+            if(board[(enemy.getX()+potentialX*i)/5][(enemy.getY()+potentialY*i)/5] != 0){
+                stt = false;
+                break;
+            }
+        }
 
-
+        return stt;
+    }
+    public int xSpeed(int dc){
+        if(dc == LEFT){
+            return -5;
+        }
+        else if (dc == RIGHT){
+            return 5;
+        }
+        return 0;
+    }
+    public int ySpeed(int dc){
+        if(dc == UP){
+            return -5;
+        }
+        else if (dc == DOWN){
+            return 5;
+        }
+        return 0;
+    }
     public static int randint(int low, int high){
 		return (int)(Math.random()*(high-low+1)+low);
 	}
@@ -251,43 +322,50 @@ class GamePanel1 extends JPanel{
 class bike{//CLASS MADE------------------------------------------------------------------------------------------------
     public static final int LEFT = 1, RIGHT = 2, UP = 3, DOWN =4;
     public static final int pixelOffset = 10;
+    public static final int speed = 5;
     public final int width = 800, height = 600;
     private int x, y, xdir, ydir;
     private int dir;
-    private int leftBorderEdge = 30, rightBorderEdge = 10, topBorderEdge = 10, botBorderEdge = 5;
+    private int leftBorderEdge = 20, rightBorderEdge = 20, topBorderEdge = 20, botBorderEdge = 20;
     private boolean yBorder = false, xBorder = true;
-    private Queue<Integer> queuedMoves = new LinkedList<>();
-    int curMove = 0;
+    // private Queue<Integer> queuedMoves = new LinkedList<>();
+    private int moveFrame = 0;
+    private int curMove = 0;
+
     public bike(int x, int y, int dir){
         this.x = x;
         this.y = y;
         if(dir == LEFT){
             this.dir = LEFT;
-            this.xdir = -5;
+            this.xdir = -speed;
         }
         if(dir == RIGHT){
             this.dir = RIGHT;
-            this.xdir = 5;
+            this.xdir = speed;
         }
     }
-    public void move(){
+    public void move(){    
         x+=xdir;
         y+=ydir;
+        
     }
     public void addDir(int d){
-        if(queuedMoves.size()<2){
-            queuedMoves.add(d);
-        }
+        curMove = d;
     }
-    public void setDir(){
+    public void setDir(){//CHANGEED MOVEMENTS (MERGE)
+        // if(curMove == 0){
+        //     if(queuedMoves.size()>0){
+        //         curMove = queuedMoves.remove();
+        //     }
+        //     else{
+        //         return;
+        //     }
+        // }
+        System.out.println(curMove);
         if(curMove == 0){
-            if(queuedMoves.size()>0){
-                curMove = queuedMoves.remove();
-            }
-            else{
-                return;
-            }
+            return;
         }
+
         if(curMove == LEFT || curMove == RIGHT){
             if(y%pixelOffset==0){
                 dirChange(curMove);
@@ -302,44 +380,48 @@ class bike{//CLASS MADE---------------------------------------------------------
         }
     }
     public void dirChange(int d){
+        System.out.println(d);
         if(d==LEFT && dir != RIGHT){
-            xdir = -5;
+            xdir = -speed;
             ydir = 0;
             dir = LEFT;
         }
         else if(d==RIGHT && dir != LEFT){
-            xdir = 5;
+            xdir = speed;
             ydir = 0;
             dir = RIGHT;
         }
         else if(d==UP && dir != DOWN){
-            ydir = -5;
+            ydir = -speed;
             xdir = 0;
             dir = UP;
         }
         else if(d==DOWN && dir != UP){
-            ydir = 5;
+            ydir = speed;
             xdir = 0;
             dir = DOWN;
         }
         else if(d ==-1){
-            System.out.println("ERROR, UNABLE TO GET WHICH BORDER THE ENEMY IS ON");
+            // System.out.println("ERROR, UNABLE TO GET WHICH BORDER THE ENEMY IS ON");
         }
     }
-    public void changeDir(){
+    public int changeDir(){
         int ch = randint(0, 1);
+        int dchange = 0;
         if(dir == UP || dir == DOWN){
-            dir = (ch == 0) ? LEFT:RIGHT;
+            dchange = (ch == 0) ? LEFT:RIGHT;
         }
         else if(dir == LEFT || dir == RIGHT){
-            dir = (ch == 0) ? UP:DOWN;
+            dchange = (ch == 0) ? UP:DOWN;
         }
-        addDir(dir);
-        printDir();
+        if(dchange == 0){
+            // System.out.println("ERROR, UNABLE TO GET WHICH DIRECTION THE ENEMY IS ON");
+        }
+        return dchange;
     }
     public void closeToBorder(){
         if(xBorder){
-            if(x < width - leftBorderEdge && x > rightBorderEdge){
+            if(x < width - rightBorderEdge && x > leftBorderEdge){
                 xBorder = false;
             }
         }
@@ -348,20 +430,26 @@ class bike{//CLASS MADE---------------------------------------------------------
                 yBorder = false;
             }
         }
-        if((x >= width - leftBorderEdge || x <= rightBorderEdge) && !xBorder){
-            if(y < height / 2){
+        if((x >= width - rightBorderEdge || x <= leftBorderEdge) && !xBorder){
+            System.out.println("BORDER TURN");
+            if(y < height / 2){  
                 addDir(DOWN);
+                System.out.println("TURNING DOWN");
             }
             else{
                 addDir(UP);
+                System.out.println("TURNING UP");
             }
             xBorder = true;
         }
-        if(y >= height - botBorderEdge || y <= topBorderEdge && !yBorder){
+        // System.out.println(y + " " + (height-botBorderEdge));
+        else if(y >= height - botBorderEdge || y <= topBorderEdge && !yBorder){
             if(x<width/2){
                 addDir(RIGHT);
+                System.out.println("TURNING RIGHT");
             }
             else{
+                System.out.println("TURNING LEFT");
                 addDir(LEFT);
             }
             yBorder = true;
@@ -369,9 +457,11 @@ class bike{//CLASS MADE---------------------------------------------------------
     }
     public boolean onBorder(){
         if(x>= width-leftBorderEdge || x<=rightBorderEdge){
+            System.out.println("X BORDER");
             return true;
         }
         if(y>=height-botBorderEdge|| y<=topBorderEdge){
+            System.out.println("Y BORDER");
             return true;
         }
         return false;
@@ -409,11 +499,24 @@ class bike{//CLASS MADE---------------------------------------------------------
     //public int[] getArea(){
 
     //}
+    public boolean hitEdgde(){
+        System.out.println(x);
+        if(x<5 || x> width-5 || y<5 || y>height-5){
+            return true;
+        }
+        return false;
+    }
     public int getX(){
         return x;
     }
     public int getY(){
         return y;
+    }
+    public int getxDir(){
+        return xdir;
+    }
+    public int getyDir(){
+        return ydir;
     }
     public void draw(Graphics g){
         g.fillRect(x, y, 10, 10);
